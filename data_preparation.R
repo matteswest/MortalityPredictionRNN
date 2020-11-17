b29b6de80b6650e02b30641b21a4adad5072ab98
Nico Frisch <frischnico@web.de>
2020-11-17 16:02:20 +0100

    Added recursive.prediction function to data_preparation.R (not working!). Also added MinMaxScaler.

--- data_preparation.R
+++ data_preparation.R
@@ -1,5 +1,6 @@
 # Functions for data preprocessing
-# function that outputs training data set
+
+# function that outputs training data set ( x_(t,x), Y_(t,x) )
 data.preprocessing <- function(data.raw, gender, country, T0, tau0, ObsYear=1999){ 
         mort_rates <- data.raw[which((data.raw$Gender == gender) & (data.raw$Country == country)), c("Year", "Age", "log_mortality")] 
         mort_rates <- dcast(mort_rates, Year ~ Age, value.var = "log_mortality")
@@ -26,3 +27,29 @@ data.preprocessing <- function(data.raw, gender, country, T0, tau0, ObsYear=1999
         }
         list(xt.train, YT.train)
 }
+
+
+# Recursive Prediction for both genders (copied from 
+# https://github.com/JSchelldorfer/ActuarialDataScience/blob/master/6%20-%20Lee%20and%20Carter%20go%20Machine%20Learning%20Recurrent%20Neural%20Networks/00_c%20package%20-%20data%20preparation%20RNNs.R)
+
+recursive.prediction.Gender <- function(ObsYear, all_mort2, gender, T0, tau0, x.min, x.max, model.p){       
+        single.years <- array(NA, c(2016-ObsYear))
+        
+        for (ObsYear1 in ((ObsYear+1):2016)){
+                data2 <- data.preprocessing(all_mort2[which(all_mort2$Year >= (ObsYear1-10)),], gender, T0, tau0, ObsYear1)
+                # MinMaxScaler (with minimum and maximum from above)
+                x.vali <- array(2*(data2[[1]]-x.min)/(x.min-x.max)-1, dim(data2[[1]]))
+                if (gender=="Female"){yy <- 0}else{yy <- 1}
+                x.vali <- list(x.vali, rep(yy, dim(x.vali)[1]))
+                y.vali <- -data2[[2]]
+                Yhat.vali2 <- exp(-as.vector(model.p %>% predict(x.vali)))
+                single.years[ObsYear1-ObsYear] <- round(10^4*mean((Yhat.vali2-exp(-y.vali))^2),4)
+                predicted <- all_mort2[which(all_mort2$Year==ObsYear1),]
+                keep <- all_mort2[which(all_mort2$Year!=ObsYear1),]
+                predicted$logmx <- -as.vector(model %>% predict(x.vali))
+                predicted$mx <- exp(predicted$logmx)
+                all_mort2 <- rbind(keep,predicted)
+                all_mort2 <- all_mort2[order(Gender, Year, Age),]
+        }
+        list(all_mort2, single.years)
+}                  
--- rnn_model.R
+++ rnn_model.R
@@ -11,7 +11,7 @@ T0 <- 10
 tau0 <- 5
 tau1 <- 20
 tau2 <- 15
-obsYears <- 2006
+obsYear <- 2006
 country <- "DEUT"
 
 # Load data.
@@ -47,9 +47,23 @@ for (l in 1:xx){
         y.train[(l-1)*2+2] <- - data_male[[2]][l]
 }
 
+# MinMaxScaler data pre-processing
+x.min <- min(x.train)
+x.max <- max(x.train)
+x.train <- list(array(2*(x.train-x.min)/(x.min-x.max)-1, dim(x.train)), gender.indicator)
+
 # The mean of y.train will be used as starting value for the intercept weight as it leeds to 
 # faster convergence
 y0 <- mean(y.train)
+
+
+# validation data pre-processing
+data2.Female <- data[which((data$Year > (obsYear-10))&(Gender=="Female")),]
+dataV.Female <- data2.Female
+vali.Y.Female <- dataV.Female[which(dataV.Female$Year > obsYear),]
+data2.Male <- data[which((data$Year > (obsYear-10))&(Gender=="Male")),]
+dataV.Male <- data2.Male
+vali.Y.Male <- dataV.Male[which(dataV.Male$Year > obsYear),]
 
 # Define network. Here we use two LSTM layers.
 Input <- layer_input(shape=c(T0,tau0), dtype='float32', name='Input') 
@@ -66,6 +80,16 @@ model %>% compile(optimizer = "adam", loss = "mse")
 
 #CBs <- callback_model_checkpoint(file.name, monitor = "val_loss", verbose = 0,  save_best_only = TRUE, save_weights_only = TRUE)
 {current_time <- Sys.time()
-        history <- model %>% fit(x = x.train, y = y.train, validation_split = 0.2, batch_size = 100, epochs = 250, verbose = 1)
+        history <- model %>% fit(x = x.train, y = y.train, validation_split = 0.2, batch_size = 100, epochs = 150, verbose = 1)
         Sys.time() - current_time}
 plot(history)
+
+# calculating out-of-sample loss: LC is c(Female=0.6045, Male=1.8152)
+# Female
+pred.result <- recursive.prediction.Gender(obsYear, data2.Female, "Female", T0, tau0, x.min, x.max, model)
+vali <- pred.result[[1]][which(data2.Female$Year > obsYear),]
+round(10^4*mean((vali$mx-vali.Y.Female$mx)^2),4)
+# Male
+pred.result <- recursive.prediction.Gender(obsYear, data2.Male, "Male", T0, tau0, x.min, x.max, model)
+vali <- pred.result[[1]][which(data2.Male$Year > obsYear),]
+round(10^4*mean((vali$mx-vali.Y.Male$mx)^2),4)
