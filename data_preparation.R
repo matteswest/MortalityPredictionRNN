2b53e265754e9ee8718075c22211113d1c5a1b38
matteswest <65230778+matteswest@users.noreply.github.com>
2020-11-18 10:50:17 +0100

    Revise variable names

--- data_preparation.R
+++ data_preparation.R
@@ -1,55 +1,59 @@
 # Functions for data preprocessing
 
 # function that outputs training data set ( x_(t,x), Y_(t,x) )
-data.preprocessing <- function(data.raw, gender, country, T0, tau0, ObsYear=1999){ 
+data.preprocessing <- function(data.raw, gender, country, timesteps, feature_dimension, observed_years=1999){ 
+
         mort_rates <- data.raw[which((data.raw$Gender == gender) & (data.raw$Country == country)), c("Year", "Age", "log_mortality")] 
         mort_rates <- dcast(mort_rates, Year ~ Age, value.var = "log_mortality")
         # selecting data
-        train.rates <- as.matrix(mort_rates[which(mort_rates$Year <= ObsYear),])
+        train.rates <- as.matrix(mort_rates[which(mort_rates$Year <= observed_years),])
         # adding padding at the border
-        (delta0 <- (tau0-1)/2)
+        (delta0 <- (feature_dimension-1)/2)
         if (delta0>0){
                 for (i in 1:delta0){
                         train.rates <- as.matrix(cbind(train.rates[,1], train.rates[,2], train.rates[,-1], train.rates[,ncol(train.rates)]))
                 }
         }
         train.rates <- train.rates[,-1]
-        (t1 <- nrow(train.rates)-(T0-1)-1)
-        (a1 <- ncol(train.rates)-(tau0-1)) 
+        (t1 <- nrow(train.rates)-(timesteps-1)-1)
+        (a1 <- ncol(train.rates)-(feature_dimension-1)) 
         (n.train <- t1 * a1) # number of training samples
-        xt.train <- array(NA, c(n.train, T0, tau0))
+        xt.train <- array(NA, c(n.train, timesteps, feature_dimension))
         YT.train <- array(NA, c(n.train))
         for (t0 in (1:t1)){
                 for (a0 in (1:a1)){
-                        xt.train[(t0-1)*a1+a0,,] <- train.rates[t0:(t0+T0-1), a0:(a0+tau0-1)]
-                        YT.train[(t0-1)*a1+a0] <- train.rates[t0+T0, a0+delta0]
+                        xt.train[(t0-1)*a1+a0,,] <- train.rates[t0:(t0 + timesteps - 1), a0:(a0+feature_dimension-1)]
+                        YT.train[(t0-1)*a1+a0] <- train.rates[t0 + timesteps, a0+delta0]
                 }
         }
         list(xt.train, YT.train)
+
 }
 
 
 # Recursive Prediction for both genders (copied from 
 # https://github.com/JSchelldorfer/ActuarialDataScience/blob/master/6%20-%20Lee%20and%20Carter%20go%20Machine%20Learning%20Recurrent%20Neural%20Networks/00_c%20package%20-%20data%20preparation%20RNNs.R)
 
-recursive.prediction.Gender <- function(ObsYear, all_mort2, gender, T0, tau0, x.min, x.max, model.p){       
-        single.years <- array(NA, c(2016-ObsYear))
+recursive.prediction.Gender <- function(observed_years, all_mort2, gender, timesteps, feature_dimension, x.min, x.max, model.p){
+
+        single.years <- array(NA, c(2016-observed_years))
         
-        for (ObsYear1 in ((ObsYear+1):2016)){
-                data2 <- data.preprocessing(all_mort2[which(all_mort2$Year >= (ObsYear1-10)),], gender, T0, tau0, ObsYear1)
+        for (observed_years1 in ((observed_years+1):2016)){
+                data2 <- data.preprocessing(all_mort2[which(all_mort2$Year >= (observed_years1-10)),], gender, timesteps, feature_dimension, observed_years1)
                 # MinMaxScaler (with minimum and maximum from above)
                 x.vali <- array(2*(data2[[1]]-x.min)/(x.min-x.max)-1, dim(data2[[1]]))
                 if (gender=="Female"){yy <- 0}else{yy <- 1}
                 x.vali <- list(x.vali, rep(yy, dim(x.vali)[1]))
                 y.vali <- -data2[[2]]
                 Yhat.vali2 <- exp(-as.vector(model.p %>% predict(x.vali)))
-                single.years[ObsYear1-ObsYear] <- round(10^4*mean((Yhat.vali2-exp(-y.vali))^2),4)
-                predicted <- all_mort2[which(all_mort2$Year==ObsYear1),]
-                keep <- all_mort2[which(all_mort2$Year!=ObsYear1),]
+                single.years[observed_years1-observed_years] <- round(10^4*mean((Yhat.vali2-exp(-y.vali))^2),4)
+                predicted <- all_mort2[which(all_mort2$Year==observed_years1),]
+                keep <- all_mort2[which(all_mort2$Year!=observed_years1),]
                 predicted$logmx <- -as.vector(model %>% predict(x.vali))
                 predicted$mx <- exp(predicted$logmx)
                 all_mort2 <- rbind(keep,predicted)
                 all_mort2 <- all_mort2[order(Gender, Year, Age),]
         }
         list(all_mort2, single.years)
-}                  
+
+}
--- rnn_model.R
+++ rnn_model.R
@@ -7,11 +7,11 @@ library(keras)
 source("data_preparation.R")
 
 # Set parameters.
-T0 <- 10
-tau0 <- 5
-tau1 <- 20
-tau2 <- 15
-obsYear <- 2006
+timesteps <- 10
+feature_dimension0 <- 5
+feature_dimension1 <- 20
+feature_dimension2 <- 15
+last_observed_year <- 2006
 country <- "DEUT"
 
 # Load data.
@@ -24,72 +24,70 @@ data$mortality <- exp(data$log_mortality)
 # Filter relevant countries.
 data <- data[which(data$Country %in% c("CHE", "DEUT", "DNK", "ESP", "FRATNP", "ITA", "JPN", "POL", "USA")),]
 
+# Split data into female and male.
+data_female <- data.preprocessing(data, "Female", country, timesteps, feature_dimension0, last_observed_year)
+data_male <- data.preprocessing(data, "Male", country, timesteps, feature_dimension0, last_observed_year)
 
-#### Both Genders
-data_female <- data.preprocessing(data, "Female", country,T0 = 10, tau0 = 5, obsYears)
-data_male <- data.preprocessing(data, "Male", country, T0 = 10, tau0 = 5, obsYears)
-
-# check if dimensions of male and female data match
-if ( (dim(data_female[[1]])[1] != dim(data_male[[1]])[1]) | (dim(data_female[[2]])[1] != dim(data_male[[2]])[1]) ) {
+# Check if dimensions of male and female data match.
+if ( (dim(data_female[[1]])[1] != dim(data_male[[1]])[1]) | (dim(data_female[[2]])[1] != dim(data_male[[2]])[1]) )
         stop("Shapes of female and male are not the same!")
-}
 
-# merge female and male data into one set
-xx <- dim(data_female[[1]])[1]
-x.train <- array(NA, dim=c(2*xx, dim(data_female[[1]])[c(2,3)]))
-y.train <- array(NA, dim=c(2*xx))
-gender.indicator <- rep(c(0,1), xx)
-for (l in 1:xx){
-        x.train[(l-1)*2+1,,] <- data_female[[1]][l,,]
-        x.train[(l-1)*2+2,,] <- data_male[[1]][l,,]
+# Merge female and male data into one set.
+sample_number <- dim(data_female[[1]])[1]
+x_train <- array(NA, dim=c(2*sample_number, dim(data_female[[1]])[c(2,3)]))
+y_train <- array(NA, dim=c(2*sample_number))
+gender_indicator <- rep(c(0,1), sample_number)
+for (l in 1:sample_number){
+        x_train[(l-1)*2+1,,] <- data_female[[1]][l,,]
+        x_train[(l-1)*2+2,,] <- data_male[[1]][l,,]
         # Invert label sign.
-        y.train[(l-1)*2+1] <- - data_female[[2]][l]
-        y.train[(l-1)*2+2] <- - data_male[[2]][l]
+        y_train[(l-1)*2+1] <- - data_female[[2]][l]
+        y_train[(l-1)*2+2] <- - data_male[[2]][l]
 }
 
-# MinMaxScaler data pre-processing
-x.min <- min(x.train)
-x.max <- max(x.train)
-x.train <- list(array(2*(x.train-x.min)/(x.min-x.max)-1, dim(x.train)), gender.indicator)
+# MinMaxScaler data pre-processing.
+x_min <- min(x_train)
+x_max <- max(x_train)
+x_train <- list(array(2*(x_train-x_min)/(x_min-x_max)-1, dim(x_train)), gender_indicator)
 
-# The mean of y.train will be used as starting value for the intercept weight as it leeds to 
-# faster convergence
-y0 <- mean(y.train)
+# The mean of y_train will be used as starting value for the intercept weight as it leeds to 
+# faster convergence.
+average_label <- mean(y_train)
 
-
-# validation data pre-processing
-data2.Female <- data[which((data$Year > (obsYear-10))&(Gender=="Female")),]
-dataV.Female <- data2.Female
-vali.Y.Female <- dataV.Female[which(dataV.Female$Year > obsYear),]
-data2.Male <- data[which((data$Year > (obsYear-10))&(Gender=="Male")),]
-dataV.Male <- data2.Male
-vali.Y.Male <- dataV.Male[which(dataV.Male$Year > obsYear),]
+# Validation data pre-processing.
+data2_female <- data[which((data$Year > (last_observed_year-timesteps)) & (Gender=="Female")),]
+x_val_female <- data2_female
+y_val_female <- x_val_female[which(x_val_female$Year > last_observed_year),]
+data2.Male <- data[which((data$Year > (last_observed_year-timesteps)) & (Gender=="Male")),]
+x_val_male <- data2.Male
+y_val_male <- x_val_male[which(x_val_male$Year > last_observed_year),]
 
 # Define network. Here we use two LSTM layers.
-Input <- layer_input(shape=c(T0,tau0), dtype='float32', name='Input') 
-Output = Input %>%  
-        layer_lstm(units = tau1, activation = 'tanh', recurrent_activation = 'tanh', 
-                         return_sequences = TRUE, name = 'LSTM1') %>%
-        layer_lstm(units = tau2, activation='tanh', recurrent_activation ='tanh', name='LSTM2') %>%           
-        layer_dense(units = 1, activation = NULL, name = "Output",
-                    weights = list(array(0,dim = c(tau2,1)), array(log(y0),dim = c(1))))  
+Input <- layer_input(shape=c(timesteps, feature_dimension0), dtype = 'float32', name = 'Input') 
+Output = Input %>%
+         layer_lstm(units = feature_dimension1, activation = 'tanh', recurrent_activation = 'tanh', 
+                    return_sequences = TRUE, name = 'LSTM1') %>%
+         layer_lstm(units = feature_dimension2, activation='tanh', recurrent_activation ='tanh', name='LSTM2') %>%
+         layer_dense(units = 1, activation = NULL, name = "Output",
+                     weights = list(array(0,dim = c(feature_dimension2,1)), array(log(average_label),dim = c(1))))
 model <- keras_model(inputs = list(Input), outputs = c(Output))
 
 # Compile network.
 model %>% compile(optimizer = "adam", loss = "mse")
 
+# TODO: Use callbacks.
 #CBs <- callback_model_checkpoint(file.name, monitor = "val_loss", verbose = 0,  save_best_only = TRUE, save_weights_only = TRUE)
 {current_time <- Sys.time()
-        history <- model %>% fit(x = x.train, y = y.train, validation_split = 0.2, batch_size = 100, epochs = 150, verbose = 1)
+        history <- model %>% fit(x = x_train, y = y_train, validation_split = 0.2, batch_size = 100, epochs = 150, verbose = 1)
         Sys.time() - current_time}
 plot(history)
 
 # calculating out-of-sample loss: LC is c(Female=0.6045, Male=1.8152)
 # Female
-pred.result <- recursive.prediction.Gender(obsYear, data2.Female, "Female", T0, tau0, x.min, x.max, model)
-vali <- pred.result[[1]][which(data2.Female$Year > obsYear),]
-round(10^4*mean((vali$mx-vali.Y.Female$mx)^2),4)
-# Male
-pred.result <- recursive.prediction.Gender(obsYear, data2.Male, "Male", T0, tau0, x.min, x.max, model)
-vali <- pred.result[[1]][which(data2.Male$Year > obsYear),]
-round(10^4*mean((vali$mx-vali.Y.Male$mx)^2),4)
+#pred.result <- recursive.prediction.Gender(last_observed_year, data2_female, "Female", timesteps, feature_dimension0, x_min, x_max, model)
+#vali <- pred.result[[1]][which(data2_female$Year > last_observed_year),]
+#round(10^4*mean((vali$mx-y_val_female$mx)^2),4)
+## Male
+#pred.result <- recursive.prediction.Gender(last_observed_year, data2.Male, "Male", timesteps, feature_dimension0, x_min, x_max, model)
+#vali <- pred.result[[1]][which(data2.Male$Year > last_observed_year),]
+#round(10^4*mean((vali$mx-y_val_male$mx)^2),4)
