9d31545f0936316cf0728ce52799d2baf5bb619b
Nico Frisch <frischnico@web.de>
2020-12-02 18:35:56 +0100

    Added grid search.

--- grid_search.R
+++ grid_search.R
@@ -2,6 +2,15 @@
 
 # Import libraries
 library(data.table)
+library(keras)
+library(tensorflow)
+use_session_with_seed(10, disable_gpu = TRUE, disable_parallel_cpu = FALSE, quiet = FALSE)
+
+library(tfruns)
+
+# Source R Scripts
+source("data_preparation.R")
+
 
 # fixed global parameters
 last_observed_year <- 2006
@@ -9,17 +18,33 @@ countries <- c("CHE", "DEUT", "DNK", "ESP", "FRATNP", "ITA", "JPN", "POL", "USA"
 
 # global parameters
 set.seed(10)
+# parameters <- list(
+#         model_type = c("LSTM", "GRU"),
+#         timesteps = c(5, 10, 15),
+#         age_range = c(5, 7, 9),
+#         layers = c(2, 3, 4),
+#         feature_dimension0 = c(20, 30, 40),
+#         feature_dimension1 = c(15, 25, 35),
+#         feature_dimension2 = c(10, 20, 30),
+#         feature_dimension3 = c(5, 15, 20),
+#         #feature_dimension4 = c(5, 10),
+#         batch_size = c(100),
+#         activation = c("tanh", "relu"),
+#         recurrent_activation = c("tanh", "sigmoid")
+# )
+
 parameters <- list(
-        timesteps = c(5, 10, 15),
-        age_range = c(5, 7, 9),
-        layers = c(2, 3, 4),
-        feature_dimension0 = c(20, 30, 40),
-        feature_dimension1 = c(15, 25, 35),
-        feature_dimension2 = c(10, 20, 30),
-        feature_dimension3 = c(5, 15, 20),
+        model_type = c("LSTM", "GRU"),
+        timesteps = c(10),
+        age_range = c(5),
+        layers = c(3),
+        feature_dimension0 = c(20),
+        feature_dimension1 = c(15),
+        feature_dimension2 = c(10),
+        feature_dimension3 = c(5),
         #feature_dimension4 = c(5, 10),
         batch_size = c(100),
-        activation = c("tanh", "relu"),
+        activation = c("tanh"),
         recurrent_activation = c("tanh", "sigmoid")
 )
 
@@ -39,19 +64,34 @@ data <- data[which(data$Country %in% countries),]
 for (timesteps in parameters$timesteps){
         for (age_range in parameters$age_range){
                 
-                # Build training data
-                combined_training_set <- create_training_data(data, countries, timesteps, age_range, last_observed_year)
-                saveRDS(combined_training_set, file = paste0("./data/training_data/training_set_", timesteps, "_", age_range, ".rds"))
+                if (!file.exists(paste0("./data/training_data/training_set_", timesteps, "_", age_range, ".rds"))) {
+                        print("Create missing training data set...")
+                        # Build training data
+                        combined_training_set <- create_training_data(data, countries, timesteps, age_range, last_observed_year)
+                        saveRDS(combined_training_set, file = paste0("./data/training_data/training_set_", timesteps, "_", age_range, ".rds"))
+                } else {
+                        print("Training data already exists.")
+                }
                 
         }
 }
 
 
+# delete grid_search directory if exists
+if (file.exists("./grid_search")){
+        unlink("./grid_search", recursive = TRUE)
+}
 
+runs <- tuning_run('hyperparameters.R', runs_dir = 'grid_search', sample = 0.5, flags = parameters)
 
+results <- ls_runs(order = metric_val_loss, decreasing= F, runs_dir = 'grid_search')
 
+library(dplyr)
+results <- select(results, -c(output))
+results
 
-
+# write to xlsx
+writexl::write_xlsx(results, "./data/results.xlsx")
 
 
 
--- hyperparameters.R
+++ hyperparameters.R
@@ -2,50 +2,73 @@
 # and specifies the flags for the construction of various models from which we want to choose 
 # the best performing one.
 
+# Import libraries
+#library(keras)
+#library(data.table)
+
 source("create_model.R")
+
+print(getwd())
+
 
 FLAGS <- flags(
         flag_string('model_type', 'LSTM'),
         flag_integer('timesteps', 10),
         flag_integer('age_range', 5),
+        flag_integer('layers', 3),
         flag_integer('feature_dimension0', 20),
         flag_integer('feature_dimension1', 15),
         flag_integer('feature_dimension2', 10),
+        flag_integer('feature_dimension3', 5),
         #flag_numeric('dropout', 0.05),
         #flag_numeric('lr', 0.01),
         #flag_integer('patience', 35),
         #flag_integer('pats', 20),
-        flag_integer('batchsize', 100), # maybe higher?
+        flag_integer('batch_size', 100), # maybe higher?
         flag_string('activation', 'tanh'),
         flag_string('recurrent_activation', 'tanh')
 )
 
-####### TODO: Data Preparation
+# Data Preparation
 # read RDS file
+combined_training_set <- readRDS(file = paste0("./data/training_data/training_set_", FLAGS$timesteps, "_", FLAGS$age_range, ".rds"))
+x_train <- combined_training_set[[1]]
+y_train <- combined_training_set[[2]]
+rm(combined_training_set)
+
+# The mean of y_train will be used as starting value for the intercept weight as it leeds to 
+# faster convergence.
+average_label <- mean(y_train)
+
 
 # construct unit sizes vector
-unit_sizes <- c(FLAGS$feature_dimension0, FLAGS$feature_dimension1, FLAGS$feature_dimension2)
+unit_sizes <- c(FLAGS$feature_dimension0, FLAGS$feature_dimension1, FLAGS$feature_dimension2, FLAGS$feature_dimension3)
+unit_sizes <- unit_sizes[1:FLAGS$layers]
 
 # Create the wanted model.
-if (model_type == "LSTM") {
-        model <- create_lstm_model(c(FLAGS$timesteps, FLAGS$age_range), unit_sizes, "tanh", "sigmoid", average_label)
+if (FLAGS$model_type == "LSTM") {
+        model <- create_lstm_model(c(FLAGS$timesteps, FLAGS$age_range), unit_sizes, FLAGS$activation, FLAGS$recurrent_activation, average_label)
 } else
-        model <- create_gru_model(c(FLAGS$timesteps, FLAGS$age_range), unit_sizes, "tanh", "sigmoid", average_label)
+        model <- create_gru_model(c(FLAGS$timesteps, FLAGS$age_range), unit_sizes, FLAGS$activation, FLAGS$recurrent_activation, average_label)
 
 
-# compile model (or do compiling step in create_model.R)
-######
+# compile model
+optimizer <- optimizer_adam()
+model %>% compile(optimizer = optimizer, loss = "mse", metrics = list("mae"))
 
 # early stopping
-early_stop <- callback_early_stopping(monitor = 'val_loss', patience = 25)
+early_stop <- callback_early_stopping(monitor = 'val_loss', patience = 35)
 
 # adaptive learning rate
 lr_reducer <- callback_reduce_lr_on_plateau(monitor = "val_loss", factor = 0.1,
                                             patience = 25, verbose = 0, mode = "min",
                                             min_delta = 1e-03, cooldown = 0, min_lr = 0)
 
-# fit model
-history <- model %>% fit(x = x_train, y = y_train, validation_split = 0.2, batch_size = 100, epochs = 100, verbose = 1, callbacks = callback_list)
+# use best model only
+use_best_model <- callback_model_checkpoint("best_model.h5", monitor = "val_loss", verbose = 1,
+                                            save_best_only = TRUE, save_weights_only = FALSE)
 
-# save model as hdf5-file
-save_model_hdf5(model, 'model.h5')
+# fit model
+history <- model %>% fit(x = x_train, y = y_train, validation_split = 0.1, batch_size = FLAGS$batch_size, epochs = 20, verbose = 1,
+                         callbacks = list(early_stop, lr_reducer, use_best_model))
+
