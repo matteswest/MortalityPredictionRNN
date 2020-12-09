f4f40dd893677212ab13c1244bb52b87b4f0e862
matteswest <65230778+matteswest@users.noreply.github.com>
2020-12-09 13:37:21 +0100

    Add output activation

--- rnn_model.R
+++ rnn_model.R
@@ -48,9 +48,9 @@ unit_sizes <- c(feature_dimension0, feature_dimension1, feature_dimension2)
 
 # Create the wanted model.
 if (model_type == "LSTM") {
-        model <- create_lstm_model(c(timesteps, age_range), unit_sizes, "tanh", "sigmoid", average_label)
+        model <- create_lstm_model(c(timesteps, age_range), unit_sizes, "tanh", "sigmoid", "exponential", average_label)
 } else
-        model <- create_gru_model(c(timesteps, age_range), unit_sizes, "tanh", "sigmoid", average_label)
+        model <- create_gru_model(c(timesteps, age_range), unit_sizes, "tanh", "sigmoid", "exponential", average_label)
 summary(model)
 
 # Compile network.
@@ -79,7 +79,7 @@ if (use_best_model) {
 
 # Fit model and measure time
 {current_time <- Sys.time()
-        history <- model %>% fit(x = x_train, y = y_train, validation_split = 0.2, batch_size = 100, epochs = 100, verbose = 1, callbacks = callback_list)
+        history <- model %>% fit(x = x_train, y = y_train, validation_split = 0.2, batch_size = 100, epochs = 10, verbose = 1, callbacks = callback_list)
 Sys.time() - current_time}
 plot_loss(model_name, history[[2]]$val_loss, history[[2]]$loss)
 
